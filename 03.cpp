44416c3746c81ab25e5a06bc3b5f94d1f7c23cbc
Robert Winke <robertwinke@gmail.com>
2016-11-02 09:46:44 +0100

    exercise 3 and 4 solved

--- 03.cpp
+++ 03.cpp
@@ -1,17 +1,36 @@
+/*
+ * 03.cpp
+ *
+ *  Created on: Nov 2, 2016
+ *      Author: robertwinke
+ */
+
+
+
 #include <iostream>
 
 using namespace std;
 
-/** 
+/**
  * Create a function that takes a pointer to a float and a float, then it should
  * multiply the value of the pointer with the given float and save the result
  * where the pointer points.
  * Please mutliply the total variable by 5 with it.
  */
 
+void multiply(float* total, float number) {
+  *total *= number;
+}
+
 int main() {
   float total = 123;
+  float* pointer = &total;
+  float number = 5;
+
+  multiply(pointer, number);
+  cout << total << endl;
 
   return 0;
 }
+
 
--- 04.cpp
+++ 04.cpp
@@ -1,13 +1,39 @@
+/*
+ * 04.cpp
+ *
+ *  Created on: Nov 2, 2016
+ *      Author: robertwinke
+ */
+
+
+
 #include <iostream>
 
 using namespace std;
 
-/** 
+/**
  * Create a function that takes an integer and returns how many divisors it has
  */
 
+int divisorCount(int number) {
+  int answer = 0;
+
+  for (int i = 1; i <= number; i++) {
+    if (number % i == 0) {
+      answer += 1;
+    }
+  }
+  return answer;
+}
+
 int main() {
+  int number;
+  cout << "Gimme a number to check" << endl;
+  cin >> number;
+
+  cout << "your number has this many divisors: " << divisorCount(number) << endl;
 
   return 0;
 }
+
 
